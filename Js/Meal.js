eec5ffc444784c7bba567eaa46467bc0fba1e92e
faysalahmmed1 <faysalahmmedcse@gmail.com>
2023-01-30 13:06:28 +0600

    my  third  commit

--- Js/Meal.js
+++ Js/Meal.js
@@ -13,12 +13,12 @@ const SearchFood = () => {
 const DisplayResultMeals = (meals)=>{
     const searchResult = document.getElementById('searchResult');
     meals.forEach(meal =>{
-        console.log(meal);
+        // console.log(meal);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML=`
             <div class="col">
-                <div class="card">
+                <div onclick = "loadMealDetail(${meal.idMeal})" class="card">
                 <img src="${meal.strMealThumb
                 }" class="card-img-top" alt="...">
                     <div class="card-body">
@@ -31,4 +31,30 @@ const DisplayResultMeals = (meals)=>{
         `;
         searchResult.appendChild(div);
     })
+}
+const loadMealDetail = mealId =>{
+    console.log(mealId);
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    fetch(url)
+    .then (res => res.json())
+    .then ( data => DisplaMealDetails(data.meals[0]));
+
+}
+const DisplaMealDetails = meal => {
+    console.log(meal);
+    const mealDetils = document.getElementById('meal-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML=`
+
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+         <div class="card-body">
+            <h5 class="card-title">${meal.strMeal}</h5>
+            <p class="card-text">${meal.strInstructions.slice(0, 100)}</p>
+             <a href="${meal.strYoutube
+             }" class="btn btn-primary">Go somewhere</a>
+        </div>
+    
+    `;
+    mealDetils.appendChild(div);
 }
